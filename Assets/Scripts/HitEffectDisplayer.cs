aa682ee64d909fec393b983dbf546a83097dfe33
Wessl <lovewessman@gmail.com>
2021-07-13 20:54:49 +0200

    add more notes to song, animate hit effects

--- Assets/Scripts/HitEffectDisplayer.cs
+++ Assets/Scripts/HitEffectDisplayer.cs
@@ -16,6 +16,8 @@ public class HitEffectDisplayer : MonoBehaviour
     private float perfectThreshold;
     private float goodThreshold;
     private float hitThreshold;
+    
+    [SerializeField] Animator animator;
 
     private void Start()
     {
@@ -27,19 +29,24 @@ public class HitEffectDisplayer : MonoBehaviour
 
     public void Hit(float dist)
     {
+        
         if (dist <= perfectThreshold)
         {
             _sr.sprite = perfect;
+            animator.Play("hiteffect", -1, 0.0f);
         } else if (dist <= goodThreshold)
         {
             _sr.sprite = good;
+            animator.Play("hiteffect", -1, 0.0f);
         } else if (dist <= hitThreshold)
         {
             _sr.sprite = hit;
+            animator.Play("hiteffect", -1, 0.0f);
         }
         else
         {
             _sr.sprite = miss;
         }
+        
     }
 }
